a22e3a16b4c1a536b5fdad4b650a725216f2433b
cherkezyan <cherkezyan@itstep.academy>
2023-09-05 09:41:03 +0300

    func1

--- second/main.cpp
+++ second/main.cpp
@@ -1,39 +1,13 @@
 #include<iostream>
 using namespace std;
-void Init(int arr[], int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		arr[i] = i + 1;
-	}
-}
-void Print(int arr[], int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
 
-void Reverse(int arr[], int size)
+void Func1()
 {
-	for (int i = size - 1; i >= 0; i--)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
+
 }
 
 int main()
 {
-	int arr[5];
-	Init(arr, 5);
-	Print(arr, 5);
-	Reverse(arr, 5);
-
-	double arr2[5];
-	Init((int*)arr2, 5);
-	Print((int*)arr2, 5);
+	
 	
 }
